2d4ebb2cdf01add14b0058a0f765d16806507239
Eyuel Ashenafi <eashenafi82@gmail.com>
2025-12-28 16:49:23 +0300

    Database for Student register Form storage

---
+++ database-mysql/student_register.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE studentdb;
+
+USE studentdb;
+
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    year INT NOT NULL
+);
